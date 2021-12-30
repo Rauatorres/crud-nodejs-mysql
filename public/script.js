4fda0faeb2294bc8c67c3ae9ae28b3821958829b
Rauatorres <rauatorres@gmail.com>
2021-12-30 20:18:17 -0300

    criando o forumlário em index.html

--- public/script.js
+++ public/script.js
@@ -2,12 +2,12 @@ fetch('http://localhost:3000/api/v1/getdata')
 .then(res => res.json())
 .then(data =>{
 	for (let index in data){
-		$('h1').html(data[0].IDPRODUTO)
 		$('table').html($('table').html() + '<tr>' +
 		'<td>' + data[index].IDPRODUTO + '</td>' +
 		'<td>' + data[index].NOME + '</td>' +
 		'<td>' + data[index].PRECO + '</td>' +
 		'<td>' + data[index].DESCRICAO + '</td>' +
+		'<td><input type="button" value="Editar" onclick=""/><input type="button" value="Excluir" onclick=""/></td>' +
 		'</tr>')
 	}
 })
